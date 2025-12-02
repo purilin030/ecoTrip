ea48d6276765ede21a7884f607d37a2063208183
huasheng2005 <luosiong0502@gmail.com>
2025-12-02 22:30:54 +0800

    add path for cancel button

--- module3/submit_proof.php
+++ module3/submit_proof.php
@@ -225,7 +225,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     </div>
 
                     <div class="flex justify-end gap-3 pt-2">
-                        <a href="../index.php"
+                        <a href="/ecotrip/module2/view_challenge.php"
                             class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-500">
                             Cancel
                         </a>
