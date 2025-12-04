798c21af16d9bbcefbd61c71434cfcec218f0c22
purilin030 <pangleeing@gmail.com>
2025-12-04 13:02:18 +0800

    module1 全部加background.php

--- background.php
+++ background.php
@@ -1,5 +1,38 @@
-<div class="fixed inset-0 -z-10 h-full w-full bg-white">
-    <div class="absolute inset-0 bg-[radial-gradient(#e5f5e0_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)] opacity-50"></div>
+<div class="fixed inset-0 -z-50 overflow-hidden bg-[#FDFCF8]">
+    <div class="absolute inset-0 opacity-40 pointer-events-none mix-blend-multiply" 
+         style="background-image: url('data:image/svg+xml,%3Csvg viewBox=%220 0 200 200%22 xmlns=%22http://www.w3.org/2000/svg%22%3E%3Cfilter id=%22noise%22%3E%3CfeTurbulence type=%22fractalNoise%22 baseFrequency=%220.8%22 numOctaves=%223%22 stitchTiles=%22stitch%22/%3E%3C/filter%3E%3Crect width=%22100%25%22 height=%22100%25%22 filter=%22url(%23noise)%22 opacity=%220.5%22/%3E%3C/svg%3E');">
+    </div>
+
+    <div class="absolute top-[-10%] left-[20%] w-[60vw] h-[60vw] bg-gradient-to-b from-amber-100/80 to-transparent rounded-full blur-[100px] opacity-80"></div>
+
+    <div class="absolute bottom-0 w-full h-[45%] lg:h-[55%]">
+        <svg class="w-full h-full drop-shadow-lg" preserveAspectRatio="none" viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg">
+            <path fill="#E6F4F1" fill-opacity="1" d="M0,192L48,197.3C96,203,192,213,288,229.3C384,245,480,267,576,250.7C672,235,768,181,864,181.3C960,181,1056,235,1152,234.7C1248,235,1344,181,1392,154.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+        </svg>
+    </div>
+
+    <div class="absolute bottom-0 w-full h-[35%] lg:h-[45%]">
+        <svg class="w-full h-full drop-shadow-xl" preserveAspectRatio="none" viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg">
+            <path fill="#CCEBE6" fill-opacity="1" d="M0,96L48,128C96,160,192,224,288,213.3C384,203,480,117,576,117.3C672,117,768,203,864,229.3C960,256,1056,224,1152,197.3C1248,171,1344,149,1392,138.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+        </svg>
+    </div>
+
+    <div class="absolute bottom-[-5%] left-[-5%] w-[40vw] max-w-[500px] h-[50vh] drop-shadow-2xl opacity-90 text-teal-800/20">
+        <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" class="w-full h-full fill-current transform -rotate-12">
+            <path d="M100 200 C 40 200, 20 150, 10 100 C 5 70, 20 40, 50 20 C 80 0, 120 0, 150 30 C 180 60, 180 120, 150 160 C 130 190, 110 200, 100 200 Z" />
+            <path d="M100 200 Q 90 150 100 50" stroke="white" stroke-width="0.5" fill="none" opacity="0.3"/>
+            <path d="M100 120 L 70 100 M 100 140 L 60 130 M 100 90 L 130 70" stroke="white" stroke-width="0.5" fill="none" opacity="0.3"/>
+        </svg>
+    </div>
+
+    <div class="absolute bottom-[-10%] right-[-10%] w-[50vw] max-w-[600px] h-[60vh] drop-shadow-2xl text-emerald-900/10">
+        <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" class="w-full h-full fill-current transform rotate-12">
+            <path d="M180 200 C 140 180, 100 160, 80 100 C 70 60, 90 20, 120 10 C 150 0, 180 40, 190 80 C 200 120, 190 180, 180 200 Z" />
+            <path d="M100 200 C 60 180, 40 140, 50 80 C 60 40, 100 20, 130 50" opacity="0.8" />
+        </svg>
+    </div>
     
-    <div class="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,255,120,0.15),rgba(255,255,255,0))]"></div>
+    <div class="absolute top-1/4 left-1/4 w-2 h-2 bg-yellow-100 rounded-full blur-[1px] opacity-60 animate-pulse"></div>
+    <div class="absolute top-1/3 right-1/3 w-1 h-1 bg-white rounded-full blur-[1px] opacity-40 animate-pulse" style="animation-delay: 1s;"></div>
+    <div class="absolute bottom-1/3 left-1/2 w-3 h-3 bg-emerald-100 rounded-full blur-[2px] opacity-30 animate-pulse" style="animation-delay: 2s;"></div>
 </div>
